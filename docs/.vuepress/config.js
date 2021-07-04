ac3a46b44e6c91ccbf6f45ea914894ea32e389f4
悠静萝莉 <i@mikuhl.cn>
2021-07-05 00:08:04 +0800

    初步撰写

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,6 +22,7 @@ module.exports = {
     markdown: {
         extendMarkdown(md) {
             md.use(require('markdown-it-task-lists'))
+            md.use(require('markdown-it-footnote'))
         }
     },
     plugins: {
@@ -31,6 +32,66 @@ module.exports = {
             updatePopup: {
                 message: "有新的内容更新啦！",
                 buttonText: "立刻刷新"
+            }
+        },
+        '@renovamen/vuepress-plugin-baidu-tongji': {
+            'ba': 'f413b820d470eb9ce7dfb8cd4863e924'
+        }
+    },
+    locales: {
+        '/': {
+            lang: 'zh-CN',
+            title: '月经应对手册'
+        },
+        '/zh-tw/': {
+            lang: 'zh-TW',
+            title: '月經應對手冊'
+        }
+    },
+    themeConfig: {
+        locales: {
+            '/': {
+                selectText: '语言',
+                label: '简体中文',
+                lang: 'zh-CN',
+                title: '月经应对手册',
+                serviceWorker: {
+                    updatePopup: {
+                        message: "有新的内容更新啦！",
+                        buttonText: "立刻刷新"
+                    }
+                },
+                nav: [
+                    { text: '主页', link: '/' }
+                ],
+                sidebar: {
+                    '/': [
+                        '/',
+                        '/menstruation-info/',
+                        '/need-do/',
+                        '/help-me/'
+                    ]
+                }
+            },
+            '/zh-tw/': {
+                selectText: '語言',
+                label: '正體中文',
+                lang: 'zh-TW',
+                title: '月經應對手冊',
+                serviceWorker: {
+                    updatePopup: {
+                        message: "有新的內容更新啦！",
+                        buttonText: "立即刷新"
+                    }
+                },
+                nav: [
+                    { text: '首頁', link: '/' }
+                ],
+                sidebar: {
+                    '/zh-tw/': [
+                        '/zh-tw/'
+                    ]
+                }
             }
         }
     }
